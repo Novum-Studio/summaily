8b9b9fbaab1e61796aac887cf3970058170f18a9
Alex <alexcw20010923@gmail.com>
2024-06-03 17:19:08 -0700

    feat: user class now has summary func

--- src/EmailSum/user.py
+++ src/EmailSum/user.py
@@ -1,19 +1,31 @@
-from provider import EmailProvider, IMAPProvider
-from dotenv import load_dotenv
+from provider import EmailProvider
+from summarizer import Summarizer
+from pathlib import Path
 
 class User:
     def __init__(self, email_provider : EmailProvider, user_id : str):
         self.email_provider = email_provider
         self.user_id = user_id
-        self.categories = ["Others"]    # Email categories after summarization (User Preferences)
-    
+        self.categories = ["Others", "Job-related"]    # Email categories after summarization (User Preferences)
+        self.summarizer = Summarizer(Path.home().joinpath('mistral_models', '7B-Instruct-v0.3'))
+
+
     def fetch_emails(self, num_emails = -1) -> list:
         self.email_provider.connect()
         result = self.email_provider.fetch_emails(num_emails)
         return result
 
-    def summarize_emails(self, num_emails = -1):
-        pass
+    def summarize_emails(self, num_emails):
+        # TODO: return dictionary in which keys are different categories.
+        for mail in self.fetch_emails(num_emails):
+            if not mail["Body"]: continue
+
+            summary = self.summarizer.summarize(mail['Body'], self.categories)
+            print(summary)
+
+
+
+        
 
         
 
